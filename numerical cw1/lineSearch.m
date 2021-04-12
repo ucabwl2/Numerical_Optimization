7ed55e882c98856acef86a7b070060c8d83bdb62
ucabwl2 <59809557+ucabwl2@users.noreply.github.com>
2021-04-12 10:56:43 +0100

    Add files via upload

---
+++ numerical cw1/lineSearch.m
@@ -0,0 +1,83 @@
+function [xMin, fMin, nIter, info] = descentLineSearch(F, descent, ls, alpha0, x0, tol, maxIter)
+% DESCENTLINESEARCH Wrapper function executing  descent with line search
+% [xMin, fMin, nIter, info] = descentLineSearch(F, descent, ls, alpha0, x0, tol, maxIter) 
+%
+% INPUTS
+% F: structure with fields
+%   - f: function handler
+%   - df: gradient handler
+%   - d2f: Hessian handler
+% descent: specifies descent direction {'steepest', 'newton'}
+% ls: function handle for computing the step length
+% alpha0: initial step length 
+% rho: in (0,1) backtraking step length reduction factor
+% c1: constant in sufficient decrease condition f(x_k + alpha_k*p_k) > f_k + c1*alpha_k*(df_k')*p_k)
+%     Typically chosen small, (default 1e-4). 
+% x0: initial iterate
+% tol: stopping condition on minimal allowed step
+%      norm(x_k - x_k_1)/norm(x_k) < tol;
+% maxIter: maximum number of iterations
+%
+% OUTPUTS
+% xMin, fMin: minimum and value of f at the minimum
+% nIter: number of iterations 
+% info: structure with information about the iteration 
+%   - xs: iterate history 
+%   - alphas: step lengths history 
+%
+% Copyright (C) 2017  Marta M. Betcke, Kiko Rullan
+
+% Parameters
+% Stopping condition {'step', 'grad'}
+stopType = 'step';
+
+% Initialization
+nIter = 0;
+x_k = x0;
+info.xs = x0;
+info.alphas = alpha0;
+stopCond = false; 
+
+% Loop until convergence or maximum number of iterations
+while (~stopCond && nIter <= maxIter)
+    
+    % ====================== YOUR CODE HERE ===================================
+    % Instructions: x_k contains the current iteration point    - used in the stopping condition
+    %               x_k_1 contains the previous iteration point - used in the stopping condition
+     if nIter>0
+        x_k_1=x_k;
+        x_k=x_k_1 + alpha0*p_k;
+        info.xs = [info.xs x_k];
+        info.alphas = [info.alphas alpha0];
+    else
+        x_k_1=x_k +100*ones(size(x_k));
+    end
+    if descent == 'steepest'
+        dx_k = F.df(x_k);
+        p_k = -dx_k / norm(dx_k);
+    elseif descent == 'newton'
+        dx_k = F.df(x_k); Hx_k = F.d2f(x_k);
+        p_k = -inv(Hx_k)* dx_k;
+    end
+    [alpha0, ~] = ls(x_k, p_k, alpha0);
+    nIter = nIter +1;
+    
+    % =========================================================================    
+    switch stopType
+      case 'step' 
+        % Compute relative step length
+        normStep = norm(x_k - x_k_1)/norm(x_k_1);
+        stopCond = (normStep < tol);
+      case 'grad'
+        stopCond = (norm(F.df(x_k), 'inf') < tol*(1 + tol*abs(F.f(x_k))));
+    end
+    
+end
+
+% Assign output values 
+xMin = x_k;
+fMin = F.f(x_k); 
+
+end
+
+
---
+++ numerical cw1/test_NewtonMethod.m
@@ -0,0 +1,73 @@
+clear all;
+clc;close all;
+% For computation define as function of 1 vector variable
+F.f = @(x) 100.*(x(2) - x(1)^2).^2 + (1 - x(1)).^2; % function handler, 2-dim vector
+F.df = @(x) [-400*(x(2) - x(1)^2)*x(1) - 2*(1 - x(1)); 
+              200*(x(2) - x(1)^2)];  % gradient handler, 2-dim vector
+F.d2f = @(x) [-400*(x(2) - 3*x(1)^2) + 2, -400*x(1); -400*x(1), 200]; % hessian handler, 2-dim vector
+% For visualisation proposes define as function of 2 variables
+rosenbrock = @(x,y) 100.*(y - x.^2).^2 + (1 - x).^2;
+
+% rosenbrock.df_dx=@(x,y) (y -x.^2).*x-2.*(1-x);
+% rosenbrock.df_dy=@(x,y) 200*(y-x.^2);
+% 
+% rosenbrock.ddf_ddx=@(x,y) -400*(y-3*x.^2)+2;
+% rosenbrock.ddf_dxdy=@(x,y) -400*x;
+% rosenbrock.ddf_ddy=@(x,y) 200;
+%%
+
+% Initialisation
+alpha0 = 1;
+maxIter = 1e4;
+alpha_max = alpha0;
+tol = 1e-6;
+%=============================
+% Point x0 = [1.2; 1.2]
+%=============================
+% x0 = [1.2; 1.2];
+x0 = [-1.2; 1]; %excerise 4c
+
+% Steepest descent line search strong WC
+lsOptsSteep.c1 = 1e-4;
+lsOptsSteep.rho = 0.1;
+lsFun = @(x_k, p_k, alpha0) backtracking(F, x_k, p_k, alpha_max, lsOptsSteep);
+[xSteep, fSteep, nIterSteep, infoSteep] = descentLineSearch(F, 'newton', lsFun, alpha0, x0, tol, maxIter)
+xMin1 = xSteep(1);
+xMin2 = xSteep(2);
+
+figure()
+plot(infoSteep.alphas)
+xlabel('iterations')
+ylabel('step size')
+title('Newton Method alphas')
+x=linspace(-1.5,1.5,1000);y=linspace(-1.5,1.5,1000);
+[X,Y] = meshgrid(x,y);
+Z=rosenbrock(X,Y);
+z=rosenbrock(infoSteep.xs(1,:),infoSteep.xs(2,:));
+figure()
+surf(X,Y,Z,'EdgeColor', 'none')
+hold on
+plot3(infoSteep.xs(1,:),infoSteep.xs(2,:),z,'r')
+xlabel('x')
+ylabel('y')
+zlabel('trajectories')
+title('Newton Method trajectories')
+
+error=[];
+for j =1:size(infoSteep.xs,2)
+    errorTemp = norm(infoSteep.xs(:,j) - xSteep)^2;
+    error = [error,errorTemp];
+end
+figure
+plot(0:length(error)-1,error)
+xlabel('# of iterations')
+ylabel('error')
+% title('Newton Method using x0= [1.2; 1.2]')
+title('Newton Method using x0= [-1.2; 1]')
+
+
+
+
+
+% assessVariableEqual('xMin1', 1, 'RelativeTolerance', 0.01)
+% assessVariableEqual('xMin2', 1, 'RelativeTolerance', 0.01)
